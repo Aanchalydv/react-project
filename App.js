c1824179dd33adde348dbbc307f643bff6696db4
Aanchalydv <157363193+Aanchalydv@users.noreply.github.com>
2024-01-22 15:45:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes,Link } from 'react-router-dom';
+import Home from './Home';
+import NewPage from './NewPage';
+import Filter from './Filter';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Link to="/NewPage">About</Link>
+      <br></br>
+      <Link to='/'>Home</Link>
+      <br></br>
+      <Link to="/Filter">Filter</Link>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/NewPage' element={<NewPage />} />
+          <Route path='/Filter' element={<Filter />} />
+        </Routes>
+      </BrowserRouter>
+  
+     
+    
+
+    </>
+  );
+}
+
+export default App;
+
